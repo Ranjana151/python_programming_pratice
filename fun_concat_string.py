d239747b748b00be986e12f25427b1cb2d91fb03
Ranjana151 <48376525+Ranjana151@users.noreply.github.com>
2019-10-03 11:24:16 +0530

    Add files via upload

---
+++ fun_concat_string.py
@@ -0,0 +1,10 @@
+#Concate two string
+def concat_str(str1,str2):
+    print("First String ",str1)
+    print("Second Sting",str2)
+    print("Concatenated Strings are",(str1+str2))
+    return
+#Main function
+str1=input("Enter the first string")
+str2=input("Enter the second string")
+concat_str(str1,str2)
